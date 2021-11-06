3ae9ea550f63c11c7ebae57890aacbd1cb4a2ef5
Felipe Tavares <felipe@sistemaquantum.com.br>
2021-11-06 09:50:58 -0300

    Correção função de formatação da data e hora mysql

--- funcoes_banco.go
+++ funcoes_banco.go
@@ -4,24 +4,29 @@ import (
 	"time"
 )
 
-// Recebe uma string com uma data e hora no formato padrão dd/mm/yyy hh:mm:ss 
+// Recebe uma string com uma data e hora no formato padrão dd/mm/yyyy hh:mm:ss 
 // e retorna uma string no formato mysql yyyy-mm-dd hh:mm:ss e um tipo error
 func FormataDataHoraMySQL(DataHora string) (string, error) {
-	dataHoraConvertida, err := time.Parse("2006-01-02T15:04:05.000-0700", DataHora)
+	formatoDataHoraEntrada := "02/01/2006 15:04:05"
+	formatoDataHoraSaida := "2006-01-02 15:04:05"
+	dataHoraConvertida, err := time.Parse(formatoDataHoraEntrada, DataHora)
 	if err != nil {
 		return "", err
 	}
 
-	return dataHoraConvertida.Format("2006-01-02 03:04:05"), nil
+	return dataHoraConvertida.Format(formatoDataHoraSaida), nil
 }
 
 // Recebe uma string com uma data e hora no padrão mysql yyyy-mm-dd hh:mm:ss 
 // e retorna uma string apenas com a data no formato yyyy-mm-dd e um tipo error
 func ExtrairDataMySQL(DataHora string) (string, error) {
-	dataHoraConvertida, err := time.Parse("2006-01-02 15:04:05", DataHora)
+	formatoDataHoraEntrada := "2006-01-02 15:04:05"
+	formatoDataHoraSaida := "2006-01-02"
+	dataHoraConvertida, err := time.Parse(formatoDataHoraEntrada, DataHora)
 	if err != nil {
 		return "", err
 	}
 
-	return dataHoraConvertida.Format("2006-01-02"), nil
+	return dataHoraConvertida.Format(formatoDataHoraSaida), nil
 }
+
